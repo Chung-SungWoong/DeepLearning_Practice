acf1fce0246150d1c8e795ad2fc012164f90b9bf
Chung SungWoong <dnd8888@naver.com>
2022-11-13 19:31:28 +0900

    Line Regression with numbers

--- Figure_LinearRegression.py
+++ Figure_LinearRegression.py
@@ -15,5 +15,30 @@ import numpy
 import pandas as pd
 import tensorflow as tf
 
+seed = 0
+numpy.random.seed(seed)
+tf.random.set_seed(seed)
 
+df = pd.read_csv('/Users/chung_sungwoong/Desktop/Practice/DeepLearning_Practice/dataset/housing.csv', delim_whitespace=True, header=None)
 
+dataset = df.values
+x = dataset[:,0:13]
+y = dataset[:,13]
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.3, random_state=seed)
+
+model = Sequential()
+model.add(Dense(30,input_dim = 13, activation = 'relu'))
+model.add(Dense(6,activation='relu'))
+model.add(Dense(1))
+
+model.compile(loss = 'mean_squared_error', optimizer = 'adam')
+
+model.fit(x_train, y_train, epochs = 200, batch_size = 10)
+
+y_prediction = model.predict(x_test).flatten()
+
+for i in range(10):
+    label = y_test[i]
+    prediction = y_prediction[i]
+    print("실제가격: {:.3f}, 예상가격: {:.3f}".format(label,prediction))
